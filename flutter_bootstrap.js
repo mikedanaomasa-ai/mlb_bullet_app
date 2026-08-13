577a19b701c5b6ecfa52533fbd081e6ae64d3772
mikedanaomasa <mikedanaomasa@example.com>
2026-08-13 23:29:22 +0900

    Update full app features

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "183854492" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "734535899" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
